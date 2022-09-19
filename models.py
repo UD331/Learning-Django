d67f22206afbfa2cf771697d5a7de3b46b9b8ddb
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-09-18 23:15:41 -0400

    Update models.py

--- models.py
+++ models.py
@@ -5,6 +5,14 @@ class Topic(models.Model):
   text = models.CharField(max_length = 200)
   date_added = models.DateTimeField(auto_now_add = True)
   
-def _str_(self):
+ class Entry(models.Model): 
   
-  return self.text
+  topic = models.ForeignKey(Topic, on_delete = models.CASCADE)
+  text = models.DateTimeField(auto_now_add=True)
+  
+  class Meta:
+    verbose_name_plural = 'entries'
+ 
+  def _str_(self):
+  
+    return self.text[:50] + "..."
