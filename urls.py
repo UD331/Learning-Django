0f9634ad97a5818349a309125d30a2132574b96a
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-10-30 15:24:35 -0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('admin/', admin.site.urls)
-  path('', include('learning_logs.urls')),
+  path('', views.index, name='index'),
 ]
